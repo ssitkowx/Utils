ed73b20beba3a03dad25c1001109aa2de9709b64
sylsit <sitkowskis@interia.pl>
2020-04-13 16:51:32 +0200

    version 1.0

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -3,7 +3,7 @@ import os, re
 
 class Conan(ConanFile):
     name            = "Utils"
-    #version         = "1.0"
+    version         = "1.0"
     license         = "freeware"
     repoUrl         = "https://github.com/ssitkowx"
     url             = repoUrl + '/' + name + '.git'
