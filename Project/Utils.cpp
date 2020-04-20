7443855e78ee34609179cb19ad18367c532f2b4e
sylsit <sitkowskis@interia.pl>
2020-04-20 22:53:39 +0200

    function asci to uint8_t, uint8_t table in hex to uint8_t tests

--- Project/Utils.cpp
+++ Project/Utils.cpp
@@ -10,18 +10,22 @@
 
 uint8_t AsciToint (const char v_asci)
 {
-    if (v_asci >= '0' && v_asci <= '9')
-        return v_asci - '0';
-    if (v_asci >= 'A' && v_asci <= 'F')
-        return v_asci - 'A' + 10;
-    if (v_asci >= 'a' && v_asci <= 'f')
-        return v_asci - 'a' + 10;
-    return -1;
+    if (v_asci >= '0' && v_asci <= '9') return v_asci - '0';
+    if (v_asci >= 'A' && v_asci <= 'F') return v_asci - 'A' + 10;
+    if (v_asci >= 'a' && v_asci <= 'f') return v_asci - 'a' + 10;
+
+    return ZERO;
 }
 
-uint8_t AsciToInt (const char v_asciTwoBytesTabInHex [TWO])
+uint8_t HexInAsciToInt (const char v_hexInAsci [TWO])
 {
-    return (AsciToint (v_asciTwoBytesTabInHex [FIRST_BYTE]) << FOUR_BITS) + AsciToint (v_asciTwoBytesTabInHex [SECOND_BYTE]);
+    char lowByte  = AsciToint (v_hexInAsci [SECOND_BYTE]);
+    if ((lowByte < ZERO) || (lowByte > FIFTEEN)) return ZERO;
+
+    char highByte = AsciToint (v_hexInAsci [FIRST_BYTE]);
+    if ((highByte < ZERO) || (highByte > FIFTEEN)) return ZERO;
+
+    return ((highByte << FOUR_BITS) + lowByte);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
