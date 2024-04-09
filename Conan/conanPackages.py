76e53cf7be287323a5a78e81e039831388d23b9f
Sylwester <sitkowskis@interia.pl>
2024-04-09 21:35:33 +0200

    support for conan 2.x

--- Conan/conanPackages.py
+++ Conan/conanPackages.py
@@ -3,67 +3,50 @@ from   conan.tools.scm   import Git
 from   conan.tools.files import replace_in_file
 
 class conanPackages:
-    def __createFolderDownload (self, vRepoPath):
-        print ('createFolderDownload')
+    def __parse (self, vPackage):
+        packageComponent = (re.split('[/]', vPackage))
+        return {'name' : packageComponent [0], 'version' : packageComponent [1] }
+    
+    def __createRepo (self, vRepoPath):
+        print ('createRepoFolder')
         if not os.path.isdir (vRepoPath):
             os.mkdir (vRepoPath)
         os.chdir (vRepoPath)
 
+    def __createPackage (self, vName, vVersion):
+        print ('createPackage')
+        self.run ('conan create . --name ' + vName + ' --version ' + vVersion)
+
+    def __updateCMakeLists (self, vProjectPath, vSearch, vReplace):
+        replace_in_file (self, os.path.join (vProjectPath, "CMakeLists.txt"), vSearch, vReplace, False)
+    
     def __cloneRepo (self, vName, vVersion, vRepoPath, vRepoUrl):
         print ('cloneRepo')
         repoUrl = vRepoUrl + '/' + vName + '.git'
         print ('url', repoUrl)
 
-        packageDownloadPath = vRepoPath + '/' + vName
-        print (packageDownloadPath)
-        if not os.path.isdir (packageDownloadPath):
+        packagePath = vRepoPath + '/' + vName
+        print (packagePath)
+        if not os.path.isdir (packagePath):
             git = Git (self)
             git.clone    (url = repoUrl, target = vName)
             git.folder = vName 
             git.checkout (commit = 'tags/' + vVersion)
 
-        os.chdir (packageDownloadPath + '/Conan')
-
-    def __createPackage (self, vUser, vChannel):
-        print ('createPackage')
-        self.run ('conan create . --user ' + vUser + ' --channel ' + vChannel)
-        
-    def __parse (self, vPackage):
-        packageComponent = (re.split('[/@]', vPackage, 3))
-        return {'name' : packageComponent [0], 'version' : packageComponent [1], 'user' : packageComponent [2], 'channel' : packageComponent [3]}
-
-    def getPaths (self, vPackagePath, vPackages):
-        print ('getPaths')
-        paths        = {}
-        packageNames = []
-        for package in vPackages:
-            print ("parse: ", package)
-            packageComponent   = conanPackages.__parse (self, package)
-            path               = vPackagePath + '/' + packageComponent ['name'] + '/' + packageComponent ['version'] + '/' + packageComponent ['user'] + '/' + packageComponent ['channel'] + '/package'
-            hashFolder         = os.listdir (path)
-            packageIncludePath = path + '/' + hashFolder [0] + '/include'
-            packageLibPath     = path + '/' + hashFolder [0] + '/lib'
-            packageName        = 'lib' + packageComponent ['name'] + 'Lib.a'
-             
-            if not os.path.isdir (packageIncludePath):
-                raise Exception ('%s. Is not package include path', packageIncludePath)
-
-            if not os.path.isdir (packageLibPath):
-                raise Exception ('%s. Is not package include lib path', packageLibPath)
-
-            paths [packageComponent ['name'] + 'PackageIncludePath'] = packageIncludePath
-            paths [packageComponent ['name'] + 'PackageLibPath']     = packageLibPath
-            paths [packageComponent ['name'] + 'PackageName']        = packageName
-            packageNames.append (packageComponent ['name'])
-
-        print (str (packageNames).strip ('[]').replace (',','').replace ('\'', ''))
-        replace_in_file (self, self.folders.root + "/CMakeLists.txt", "PackagesTempNames", str (packageNames).strip ('[]').replace (',','').replace ('\'', ''), False)
-        return paths
+        os.chdir (packagePath + '/Conan')
 
     def install (self, vRepoPath, vRepoUrl, vPackages):
         print ('install')
+
+        projectPath = os.getcwd ().replace ('/Build/Release','')
+        conanPackages.__updateCMakeLists (self, projectPath, "SET (PackageIncludePath )", "SET (PackageIncludePath " + self.packagePath + ")")
+
+        packageNames = []
         for package in vPackages:
             packageComponent = conanPackages.__parse (self, package)
-            conanPackages.__createFolderDownload     (self, vRepoPath)
+            conanPackages.__createRepo               (self, vRepoPath)
             conanPackages.__cloneRepo                (self, packageComponent ['name'], packageComponent ['version'], vRepoPath, vRepoUrl)
-            conanPackages.__createPackage            (self, packageComponent ['user'], packageComponent ['channel'])
+            conanPackages.__createPackage            (self, packageComponent ['name'], packageComponent ['version'])
+
+            packageNames.append (packageComponent ['name'])
+        conanPackages.__updateCMakeLists (self, projectPath, "SET (PackageNames )", "SET (PackageNames " + " ".join (packageNames) + ")")
--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -5,8 +5,6 @@ from conanPackages     import conanPackages
 from conan.tools.files import copy, load
 from conan.tools.cmake import CMake, cmake_layout
 
-from conan.tools.scm   import Git
-
 class Conan(ConanFile):
     name            = "utils"
     version         = "1.2"
@@ -15,70 +13,49 @@ class Conan(ConanFile):
     license         = "freeware"
     repoUrl         = "https://github.com/ssitkowx"
     url             = repoUrl + '/' + name + '.git'
-    description     = "General class for utils"
+    description     = "General class for gpio"
     settings        = "os", "compiler", "build_type", "arch"
     options         = { "shared": [True, False] }
     default_options = { "shared": False         }
     generators      = "CMakeDeps", "CMakeToolchain"
     author          = "sylsit"
     exports         = "*"
-    #exports_sources = "CMakeLists.txt", "Project/*"
-    #requires        = ["Utils/1.1@ssitkowx/stable"]
     requires        = ["gtest/cci.20210126"]
     downloadPath    = "/home/sylwester/.conan2/download"
     repoPath        = downloadPath + '/Repo'
     packagePath     = downloadPath + '/Package'
-    #packages        = ["Utils/1.1@ssitkowx/stable"]
     packages        = []
 
     def layout (self):
-        print ("Layout !!!!!!!!!!!!!!!!!!")
-        projectPath  = os.getcwd ().replace ('/Conan','')
-        print (projectPath)
-
+        projectPath = os.getcwd ().replace ('/Conan','')
         cmake_layout (self, src_folder = projectPath, build_folder = projectPath + '/Build')
 
     def source (self):
-        print ("Source !!!!!!!!!!!")
         cmake_file = load (self, "CMakeLists.txt")
-        #conanPackages.install (self, self.repoPath, self.repoUrl, self.packages)
+        conanPackages.install (self, self.repoPath, self.repoUrl, self.packages)
 
     def build (self):
-        print ("Build !!!!!!!!!!!!!!!!!!")
-        print (os.getcwd ())
-        projectPath  = os.getcwd ().replace ('/Build/Release','')
-        buildPath = projectPath
-
         if self.settings.os == 'Linux' and self.settings.compiler == 'gcc':
-            #packagePaths = conanPackages.getPaths (self, self.packagePath, self.packages)
-            cmake        = CMake (self)
-            '''
-            conanPath = self.folders.root + '/Conan/' + "/PackagesProperties.txt"
-
-            print ("Conan path !!!!!")
-            print (conanPath)
-            packagesPropertiesFileHandler = open (conanPath, "w")
-            for packagePathKey, packagePathValue in packagePaths.items ():
-                packagesPropertiesFileHandler.writelines (packagePathKey + "=" + packagePathValue + "\n")
-            packagesPropertiesFileHandler.close ()
-            '''
+            cmake = CMake (self)
             cmake.configure ()
             cmake.build     ()
         else:
             raise Exception ('Unsupported platform or compiler')
 
     def package (self):
-        print ("Package !!!!")
         packagePath = self.packagePath + '/' + self.name
 
-        copy (self, '*.h'  , src = os.path.join (self.source_folder, "Project"), dst = os.path.join (packagePath, "include"), keep_path = False)
-        copy (self, '*.hxx', src = os.path.join (self.source_folder, "Project"), dst = os.path.join (packagePath, "include"), keep_path = False)
-        copy (self, '*.a'  , src = self.build_folder                           , dst = os.path.join (packagePath, "lib")    , keep_path = False)
+        copy (self, '*.h'  , src = os.path.join (self.source_folder, "Project"), dst = os.path.join (packagePath, "include")        , keep_path = False)
+        copy (self, '*.hxx', src = os.path.join (self.source_folder, "Project"), dst = os.path.join (packagePath, "include")        , keep_path = False)
+        copy (self, '*.a'  , src = self.build_folder                           , dst = os.path.join (packagePath, "lib")            , keep_path = False)
+
+        copy (self, '*.h'  , src = os.path.join (self.source_folder, "Project"), dst = os.path.join (self.package_folder, "include"), keep_path = False)
+        copy (self, '*.hxx', src = os.path.join (self.source_folder, "Project"), dst = os.path.join (self.package_folder, "include"), keep_path = False)
+        copy (self, '*.a'  , src = self.build_folder                           , dst = os.path.join (self.package_folder, "lib")    , keep_path = False)
 
     def export_sources (self):
-        print ("Export sources !!!!!!!!!!!")
-
         receipePath = os.path.join (self.recipe_folder, "..")
+
         copy (self, "*.txt"        , receipePath, self.export_sources_folder)
         copy (self, "Tests/*.hxx"  , receipePath, self.export_sources_folder)
         copy (self, "Tests/*.cxx"  , receipePath, self.export_sources_folder)
