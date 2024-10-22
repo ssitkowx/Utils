28a6374645b8c33fa83855e3012548c6f3d1b29f
sylwester.sitkowski <sylwester.sitkowski@gunnebo.com>
2024-10-22 10:43:21 +0200

    Remove circular dependency

--- Tests/UtilsFixture.cxx
+++ Tests/UtilsFixture.cxx
@@ -14,35 +14,35 @@
 
 TEST_F (UtilsFixture, CheckAsciTointPositiveConversion)
 {
-    LOGW (Module, "CheckAsciTointPositiveConversion");
+    std::cout << Module << " CheckAsciTointPositiveConversion\n";
 
     for (uint8_t charNum = ZERO; charNum < TWENTY_TWO; charNum++)
     {
         const int input  = AsciToint (AsciToInt.ConversionResult.Positive [charNum].first);
         const int output = AsciToInt.ConversionResult.Positive [charNum].second;
 
-        LOGD (Module, "Input asci:", AsciToInt.ConversionResult.Positive [charNum].first, ". Output int ", output);
+        std::cout << Module << " Input asci: " << AsciToInt.ConversionResult.Positive [charNum].first << ". Output int " << output  << std::endl;
         EXPECT_EQ (input, output);
     }
 }
 
 TEST_F (UtilsFixture, CheckAsciTointNegativeConversion)
 {
-    LOGW (Module, "CheckAsciTointNegativeConversion");
+    std::cout << Module << " CheckAsciTointNegativeConversion\n";
 
     for (uint8_t charNum = ZERO; charNum < TEN; charNum++)
     {
         const int input  = AsciToint (AsciToInt.ConversionResult.Negative [charNum]);
         const int output = ZERO;
 
-        LOGD (Module, "Input asci:", AsciToInt.ConversionResult.Negative [charNum], ". Output int ", output);
+        std::cout << Module << " Input asci: " << AsciToInt.ConversionResult.Negative [charNum] << ". Output int " << output << std::endl;
         EXPECT_EQ (input, output);
     }
 }
 
 TEST_F (UtilsFixture, CheckHexInAsciToIntPositiveConversion)
 {
-    LOGW (Module, "CheckHexInAsciToIntPositiveConversion");
+    std::cout << Module << "CheckHexInAsciToIntPositiveConversion\n";
 
     for (uint8_t charNum = ZERO; charNum < TWO_HUNDRED_FIFTY_FIVE; charNum++)
     {
@@ -50,14 +50,14 @@ TEST_F (UtilsFixture, CheckHexInAsciToIntPositiveConversion)
         const int  input     = HexInAsciToInt (hex);
         const int  output    = HexInAsciToIntConversionResult.Positive [charNum].second;
 
-        LOGD (Module, "Input asci: 0x", hex [ZERO] , hex [ONE], ". Output int ", output);
+        std::cout << Module << " Input asci: 0x" << hex [ZERO] << hex [ONE] << ". Output int " << output << std::endl;
         EXPECT_EQ (input, output);
     }
 }
 
 TEST_F (UtilsFixture, CheckHexInAsciToIntNegativeConversion)
 {
-    LOGW (Module, "CheckHexInAsciToIntNegativeConversion");
+    std::cout << Module << " CheckHexInAsciToIntNegativeConversion";
 
     for (uint8_t charNum = ZERO; charNum < TEN; charNum++)
     {
@@ -65,20 +65,20 @@ TEST_F (UtilsFixture, CheckHexInAsciToIntNegativeConversion)
         const  int input  = HexInAsciToInt (hex);
         const  int output = ZERO;
 
-        LOGD (Module, "Input asci: 0x", hex [ZERO] , hex [ONE], ". Output int ", output);
+        std::cout << Module << " Input asci: 0x" << hex [ZERO] << hex [ONE] <<". Output int " << output << std::endl;
         EXPECT_EQ (input, output);
     }
 }
 
 TEST_F (UtilsFixture, ConvertStringToBinaryAndReverse)
 {
-    LOGW (Module, "ConvertStringToBinary");
+    std::cout << Module << " ConvertStringToBinary\n";
 
     std::string_view string = "\\x12\\x10\\x08\\x5a\\x10\\xa6\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x01\\x18\\x8e\\x02";
     const std::string binaryData = ConvertStringToBinary (string);
     const std::string stringData = ConvertBinaryToString (binaryData);
 
-    LOGI      (Module, "binary string: ", stringData);
+    std::cout << Module << " binary string: " << stringData << std::endl;
     EXPECT_EQ (string, stringData);
 }
 
